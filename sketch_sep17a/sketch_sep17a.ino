6caaae2e5e814c02550af5f8a52dc00a897c1684
faustus <faustus@kali.kali>
2025-09-19 13:13:26 -0400

    in-progress

---
+++ sketch_sep17a/sketch_sep17a.ino
@@ -0,0 +1,32 @@
+int sensorPinIn = 2;    // Water flow sensor input
+int sensorPinOut = 13;   // Output pin to control relay
+volatile unsigned long pulse = 0; // Counts pulses from the water flow sensor
+
+void setup() {
+  pinMode(sensorPinOut, OUTPUT); // Relay pin
+  pinMode(sensorPinIn, INPUT);   // Water flow sensor pin
+  Serial.begin(9600);
+
+  // Attach interrupt to the input pin
+  attachInterrupt(digitalPinToInterrupt(sensorPinIn), increase, RISING);
+}
+
+void loop() {
+  static unsigned long lastPulseCount = 0;
+
+  // Compare current pulse count to previous
+  if (pulse > lastPulseCount) {
+    digitalWrite(sensorPinOut, HIGH); // Turn on relay
+    lastPulseCount = pulse;
+  } else {
+    digitalWrite(sensorPinOut, LOW); // Turn off relay if no new pulses
+  }
+
+  // Print current pulse count
+  Serial.println(pulse);
+  delay(50);
+}
+
+void increase() {
+  pulse++;
+}
